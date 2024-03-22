0880057e5178542bc3cf0091a382d09acfd8cab2
ByteBigBoss <nethminas42@gmail.com>
2024-03-22 22:48:04 +0530

    setup env of app

---
+++ pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+   <Button>Hello Admin!</Button>
+  )
+}
+
+export default HomePage
